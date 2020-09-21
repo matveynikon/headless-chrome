c4934bafdff61765d78c97a9faa1a3361707586d
Matthew <matvey.nikon@gmail.com>
2020-09-21 21:56:21 +0200

    jeff
    gggg

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
-const express = require('express');
-const app = express();
-const puppeteer = require('puppeteer');
-const port = process.env.PORT || 8080;
-const validUrl = require('valid-url');
-
-var parseUrl = function(url) {
-    url = decodeURIComponent(url)
-    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-        url = 'http://' + url;
-    }
-
-    return url;
-};
-
-app.get('/', function(req, res) {
-    var urlToScreenshot = parseUrl(req.query.url);
-
-    if (validUrl.isWebUri(urlToScreenshot)) {
-        console.log('Screenshotting: ' + urlToScreenshot);
-        (async() => {
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
-            });
-
-            const page = await browser.newPage();
-            await page.goto(urlToScreenshot);
-            await page.screenshot().then(function(buffer) {
-                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
-                res.setHeader('Content-Type', 'image/png');
-                res.send(buffer)
-            });
-
-            await browser.close();
-        })();
-    } else {
-        res.send('Invalid url: ' + urlToScreenshot);
-    }
-
-});
-
-app.listen(port, function() {
-    console.log('App listening on port ' + port)
-})
+const puppeteer = require('puppeteer')
+console.log('dude')
+async function start () {
+  const url = 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean'
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox',
+      '--disable-setuid-sandbox']
+  })
+  const page = await browser.newPage()
+  page.setDefaultNavigationTimeout(0)
+  await page.goto(url)
+  await page.setViewport({ width: 1100, height: 700 })
+  await page.keyboard.type(' ')
+  await page.waitFor(5000)
+  await page.close()
+  browser.close()
+}
+start()
